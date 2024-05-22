719e9c2b00f61552776438d7c71aca36eadf8ecf
Gia <maqandarashvili@gmail.com>
2024-05-22 12:04:07 +0400

    Changed favicon

--- app/Modules/Admin/Resources/Views/layouts/layout.blade.php
+++ app/Modules/Admin/Resources/Views/layouts/layout.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="robots" content="noindex, nofollow">
-    <link rel="shortcut icon" href="img/favicon.png">
+{{--    <link rel="shortcut icon" href="img/favicon.png">--}}
+    <link rel="shortcut icon" href="favicon.ico">
     <base href="{{ URL::to('/') }}/">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('admin.project_name') }}</title>
