b2d13fb70a43ba466dd170aafb01ab3fb8da61c9
Gia <maqandarashvili@gmail.com>
2025-04-07 23:42:43 +0400

    Image link previews

--- resources/js/base/admin/lead/partials/LeadComments.vue
+++ resources/js/base/admin/lead/partials/LeadComments.vue
@@ -1,54 +1,106 @@
 <template>
     <div class="comments-section">
         <ul class="comments-list">
-            <li v-for="comment in visibleComments" :key="comment.id"
-                :class="['comment-item',{ mine: comment.admin_id === userId }]">
+            <li
+                v-for="comment in visibleComments"
+                :key="comment.id"
+                :class="['comment-item',{ mine: comment.admin_id === userId }]"
+            >
                 <div class="comment-header">
-                    <span class="comment-user" v-if="comment.author_name">{{ comment.author_name }}</span>
-                    <span class="comment-user" v-else-if="comment.admin">{{ comment.admin.name }} {{
-                            comment.admin.surname
-                        }}</span>
-                    <span class="comment-user" v-else-if="comment.investor">{{
-                            comment.investor.name
-                        }} {{ comment.investor.surname }}</span>
-                    <span class="comment-date">{{ formatDate(comment.created_at) }}</span>
+          <span class="comment-user" v-if="comment.author_name">
+            {{ comment.author_name }}
+          </span>
+                    <span class="comment-user" v-else-if="comment.admin">
+            {{ comment.admin.name }} {{ comment.admin.surname }}
+          </span>
+                    <span class="comment-user" v-else-if="comment.investor">
+            {{ comment.investor.name }} {{ comment.investor.surname }}
+          </span>
+                    <span class="comment-date">
+            {{ formatDate(comment.created_at) }}
+          </span>
                 </div>
                 <p class="comment-text">{{ comment.comment }}</p>
-                <a v-if="comment.attachment" :href="`${comment.attachment}`" target="_blank"
-                   class="comment-attachment">View
-                    {{ getFilename(comment.attachment) }}</a>
+
+                <!-- Render a basic link preview if available -->
+                <div v-if="comment.preview" class="link-preview">
+                    <a :href="comment.preview.ogTitle" target="_blank">
+                        <img
+                            v-if="comment.preview.ogImage"
+                            :src="comment.preview.ogImage"
+                            alt="Link preview image"
+                            class="link-preview-image"
+                        />
+                        <div v-else class="link-preview-info">
+                            {{ comment.preview.ogTitle }}
+                        </div>
+                    </a>
+                </div>
+
+                <a
+                    v-if="comment.attachment"
+                    :href="`${comment.attachment}`"
+                    target="_blank"
+                    class="comment-attachment"
+                >
+                    View {{ getFilename(comment.attachment) }}
+                </a>
                 <div class="comment-actions">
-                    <span class="comment-delete" @click="deleteComment(comment.id)">
-                        Delete
-                    </span>
+          <span class="comment-delete" @click="deleteComment(comment.id)">
+            Delete
+          </span>
                 </div>
             </li>
         </ul>
-        <el-button v-if="!showAllComments && hasOlderComments" @click="showAllComments = true"
-                   class="show-all-comments-button">Show All Comments
+        <el-button
+            v-if="!showAllComments && hasOlderComments"
+            @click="showAllComments = true"
+            class="show-all-comments-button"
+        >
+            Show All Comments
         </el-button>
         <el-form @submit.prevent="submitComment" class="comment-form">
-            <el-input type="textarea" v-model="newComment" placeholder="Add a comment" class="comment-input"></el-input>
+            <el-input
+                type="textarea"
+                v-model="newComment"
+                placeholder="Add a comment"
+                class="comment-input"
+            ></el-input>
             <div class="end-comments-form">
                 <div>
                     <label for="comment-file-input" class="custom-file-label">
                         <i class="el-icon-upload"></i> Upload File
                     </label>
-                    <input type="file" id="comment-file-input" @change="onFileChange" class="comment-file-input">
-                    <span class="file-name">{{ attachmentName || 'No file chosen' }}</span>
+                    <input
+                        type="file"
+                        id="comment-file-input"
+                        @change="onFileChange"
+                        class="comment-file-input"
+                    />
+                    <span class="file-name">
+            {{ attachmentName || 'No file chosen' }}
+          </span>
                 </div>
-                <el-button type="primary" @click="submitComment" class="comment-submit-button">Submit</el-button>
+                <el-button
+                    type="primary"
+                    @click="submitComment"
+                    class="comment-submit-button"
+                >
+                    Submit
+                </el-button>
             </div>
         </el-form>
     </div>
 </template>
 
 <script>
-import {responseParse} from '../../../mixins/responseParse';
-import {getData} from '../../../mixins/getData';
+import { responseParse } from '../../../mixins/responseParse';
+import { getData } from '../../../mixins/getData';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import axios from 'axios';
 
 export default {
+    name: "CommentsComponent",
     props: {
         id: {
             type: [String, Number],
@@ -57,7 +109,7 @@ export default {
         userId: {
             type: [String, Number],
             required: true
-        },
+        }
     },
     data() {
         return {
@@ -78,12 +130,16 @@ export default {
             }
             const oneMonthAgo = new Date();
             oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
-            return this.comments.filter(comment => new Date(comment.created_at) >= oneMonthAgo);
+            return this.comments.filter(
+                comment => new Date(comment.created_at) >= oneMonthAgo
+            );
         },
         hasOlderComments() {
             const oneMonthAgo = new Date();
             oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
-            return this.comments.some(comment => new Date(comment.created_at) < oneMonthAgo);
+            return this.comments.some(
+                comment => new Date(comment.created_at) < oneMonthAgo
+            );
         }
     },
     methods: {
@@ -97,10 +153,58 @@ export default {
                 this.attachmentName = null;
             }
         },
-        async fetchComments() {
-            await axios.get(`/lead/${this.id}/comments`).then(response => {
-                this.comments = response.data.data;
+        // Extract the first URL found in the comment text
+        extractUrl(text) {
+            const urlRegex = /(https?:\/\/[^\s]+)/g;
+            const matches = text.match(urlRegex);
+            return matches ? matches[0] : null;
+        },
+        // Process each comment to attach a basic preview if a link is detected
+        processComments() {
+            this.comments.forEach(comment => {
+                const url = this.extractUrl(comment.comment);
+                if (url) {
+                    // If the URL is a direct image link, use it as the preview image
+                    if (url.match(/\.(jpeg|jpg|gif|png)$/i)) {
+                        comment.preview = {
+                            ogImage: url,
+                            ogTitle: url
+                        };
+                    }
+                    // Check if it's a YouTube link
+                    else if (url.includes('youtube.com') || url.includes('youtu.be')) {
+                        let videoId = null;
+                        const youtubeRegex = /(?:youtube\.com\/.*(?:\?|&)v=|youtu\.be\/)([^&\n?#]+)/;
+                        const match = url.match(youtubeRegex);
+                        if (match && match[1]) {
+                            videoId = match[1];
+                        }
+                        if (videoId) {
+                            comment.preview = {
+                                ogImage: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`,
+                                ogTitle: url
+                            };
+                        } else {
+                            comment.preview = { ogTitle: url };
+                        }
+                    }
+                    // Fallback: simply display the URL as a clickable link
+                    else {
+                        comment.preview = {
+                            ogTitle: url
+                        };
+                    }
+                }
             });
+        },
+        async fetchComments() {
+            try {
+                const response = await axios.get(`/lead/${this.id}/comments`);
+                this.comments = response.data.data;
+                this.processComments();
+            } catch (error) {
+                console.error("Error fetching comments:", error);
+            }
         },
         async deleteComment(commentId) {
             this.$confirm('Are you sure?', 'You are deleting a comment', {
@@ -108,10 +212,14 @@ export default {
                 cancelButtonText: 'No',
                 type: 'warning'
             }).then(async () => {
-                await axios.post(`/lead/${this.id}/comments/delete/${commentId}`).then(response => {
+                try {
+                    const response = await axios.post(`/lead/${this.id}/comments/delete/${commentId}`);
                     responseParse(response);
                     this.comments = response.data.data;
-                });
+                    this.processComments();
+                } catch (error) {
+                    console.error("Error deleting comment:", error);
+                }
             });
         },
         async submitComment() {
@@ -120,16 +228,18 @@ export default {
             if (this.attachment) {
                 formData.append('attachment', this.attachment);
             }
-
-            let url = `/lead/${this.id}/comments`;
-            await axios.post(url, formData)
-                .then(response => {
-                    responseParse(response);
-                    this.comments = response.data.data;
-                    this.newComment = '';
-                    this.attachment = null;
-                    this.attachmentName = null;
-                });
+            const url = `/lead/${this.id}/comments`;
+            try {
+                const response = await axios.post(url, formData);
+                responseParse(response);
+                this.comments = response.data.data;
+                this.processComments();
+                this.newComment = '';
+                this.attachment = null;
+                this.attachmentName = null;
+            } catch (error) {
+                console.error("Error submitting comment:", error);
+            }
         },
         formatDate(isoString) {
             const date = new Date(isoString);
@@ -165,7 +275,7 @@ export default {
 .comment-item {
     padding: 15px 25px;
     margin-bottom: 10px;
-    background: #EFF0F1;
+    background: #eff0f1;
     border-radius: 20px;
     width: 60%;
     align-self: flex-start;
@@ -175,10 +285,9 @@ export default {
     .comment-item {
         width: 85%;
     }
-    .comment-header{
+    .comment-header {
         flex-direction: column;
     }
-
 }
 
 .comment-item.mine {
@@ -197,7 +306,7 @@ export default {
 }
 
 .comment-item.mine .comment-user {
-    color: #FF9100;
+    color: #ff9100;
 }
 
 .comment-date {
@@ -229,7 +338,7 @@ export default {
 }
 
 .comment-file-input {
-    display: none; /* Hide the default file input */
+    display: none;
 }
 
 .custom-file-label {
@@ -279,5 +388,27 @@ export default {
 .show-all-comments-button {
     display: block;
     margin: 20px auto;
+}
+
+/* Link Preview Styles */
+.link-preview {
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    margin-top: 10px;
+    overflow: hidden;
+    max-width: 300px;
+}
+.link-preview-image {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+}
+.link-preview-info {
+    padding: 10px;
+}
+.link-preview-info a {
+    text-decoration: none;
+    color: #1e90ff;
+    word-break: break-all;
 }
 </style>
