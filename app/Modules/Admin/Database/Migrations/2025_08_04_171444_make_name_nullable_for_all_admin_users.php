ad7e309a823ddeb24b8579d5cd3c34fd85621560
Gia <maqandarashvili@gmail.com>
2025-08-04 17:16:07 +0400

    Added migrations

---
+++ app/Modules/Admin/Database/Migrations/2025_08_04_171444_make_name_nullable_for_all_admin_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeNameNullableForAllAdminUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->string('name', 255)->nullable()->change();
+            $table->string('surname', 255)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            //
+        });
+    }
+}
